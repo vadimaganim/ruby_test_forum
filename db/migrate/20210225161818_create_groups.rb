d0d1343f23c42cb00c8442bb7fa60f0226f45fc5
Vadem <vadem@Vadems-Mac-mini.local>
2021-02-25 18:33:36 +0200

    Add registartion/auth and Start work with groups

---
+++ db/migrate/20210225161818_create_groups.rb
@@ -0,0 +1,11 @@
+class CreateGroups < ActiveRecord::Migration[6.1]
+  def change
+    create_table :groups do |t|
+      t.string :title
+      t.text :body
+      t.references :user, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
